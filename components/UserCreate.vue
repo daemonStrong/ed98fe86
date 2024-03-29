9efcb839956a76ea479183887b956f18d70be145
Vadim Kushir <vadim.kushnir@logity.tech>
2024-03-29 17:05:24 +0500

    feat: implemented users table

---
+++ components/UserCreate.vue
@@ -0,0 +1,157 @@
+<template>
+  <v-form v-model="valid" @submit.prevent>
+    <v-container class="bg-white">
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            :rules="nameRules"
+            label="ФИО"
+            required
+            variant="outlined"
+          />
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="4">
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+            variant="outlined"
+          />
+        </v-col>
+
+        <v-col cols="4">
+          <v-text-field
+            type="tel"
+            v-model="phone"
+            :rules="[(v) => !!v || 'Телефон обязателен']"
+            label="Телефон"
+            required
+            variant="outlined"
+          />
+        </v-col>
+
+        <v-col>
+          <v-select
+            v-model="status"
+            label="Select"
+            :items="[{title: 'Активный', value: 'ACTIVE'}, {title: 'Заблокирован', value: 'BLOCKED'}]"
+            variant="outlined"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row>
+        <v-col cols="auto">
+          <v-btn
+            variant="outlined"
+            color="#2196F3"
+            @click="back()"
+          >
+            Отменить
+          </v-btn>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn type="submit" @click="submit" :disabled="!valid && false" :loading="isLoading" color="#2196F3">
+            {{ !$route.params?.id ? 'Добавить' : 'Сохранить' }}
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    
+  </v-form>
+</template>
+
+<script lang="ts" setup>
+  interface User {
+    email: string;
+    full_name: string;
+    id: number;
+    phone: string;
+    status: 'ACTIVE' | 'BLOCKED';
+  }
+
+  const valid = ref(false)
+  const isLoading = ref(false)
+
+  const { params } = useRoute()
+  const { back } = useRouter()
+  const { data, pending, error, refresh } = await useFetch(`http://127.0.0.1:8000/api/users/${params?.id}/`, {
+    immediate: false
+  })
+
+  const name = ref('')
+  const nameRules = [
+    value => {
+      if (value) return true
+
+      return 'Name is required.'
+    },
+    value => {
+      if (value?.length >= 10) return true
+
+      return 'Полное имя должно быть длиннее 10 букв.'
+    }
+  ]
+
+  const email = ref('')
+  const emailRules = [
+    value => {
+      if (value) return true
+
+      return 'E-mail обязателен.'
+    },
+    value => {
+      if (/.+@.+\..+/.test(value)) return true
+
+      return 'неправильный E-mail.'
+    }
+  ]
+
+  const phone = ref('')
+
+  const status = ref('ACTIVE')
+
+  if(params?.id) {
+    await refresh()
+    name.value = data.value.full_name
+    email.value = data.value.email
+    phone.value = data.value.phone
+    status.value = data.value.status
+  }
+  
+  const submit = async () => {
+    isLoading.value = true
+    try {
+      const response = await $fetch(`http://127.0.0.1:8000/api/users${!params?.id ? '' : `/${params?.id}`}/`, {
+        method: !params?.id ? 'POST' : 'PUT',
+        body: {
+          // status: status.value,
+          // full_name: name.value,
+          // email: email.value,
+          // phone: phone.value
+          "full_name": "string",
+          "email": "user@example.com",
+          "phone": "string",
+          "password": "string",
+          "status": "ACTIVE",
+          "department": "string",
+          "preferred_contact_method": [
+            "SMS"
+          ]
+        }
+      })
+    } catch(error) {
+      console.error(error)
+    } finally {
+      isLoading.value = false
+      back()
+    }
+  }
+</script>
---
+++ components/UsersTable.vue
@@ -0,0 +1,137 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-table
+        height="300px"
+        fixed-header
+      >
+        <thead>
+          <tr>
+            <th class="text-left">
+              ФИО
+            </th>
+            <th class="text-left">
+              Email
+            </th>
+            <th class="text-left">
+              Телефон
+            </th>
+            <th class="text-left">
+              Статус
+            </th>
+            <th class="text-left">
+              Подразделение
+            </th>
+            <th class="text-left">
+              Канал отправки
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in users"
+            :key="item.name"
+            @click="$router.push(`/users/${item.id}`)"
+          >
+            <td>{{ item.full_name }}</td>
+            <td>{{ item.email }}</td>
+            <td>{{ item.phone }}</td>
+            <td>{{ item.department }}</td>
+            <td>{{ item?.preferred_contact_method.join(', ') }}</td>
+            <td>
+              <v-row align="center">
+                <v-col cols="9">
+                  <v-chip label :color="item.status !== 'ACTIVE' ? 'error' : 'success'">
+                    {{ item.status !== 'ACTIVE' ? 'ЗАБЛОКИРОВАН' : 'АКТИВЕН' }}
+                  </v-chip>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col>
+                  <v-menu>
+                    <template v-slot:activator="{ props }">
+                      <v-btn
+                        v-bind="props"
+                        flat
+                        slim
+                        icon="mdi-dots-vertical"
+                        size="small"
+                      />
+                    </template>
+                    <v-list>
+                      <v-list-item>
+                        <v-list-item-title @click="$router.push(`/users/${item.id}/edit/`)">
+                          Редактировать
+                        </v-list-item-title>
+                      </v-list-item>
+                      <v-list-item>
+                        <v-dialog max-width="500">
+                          <template v-slot:activator="{ props: activatorProps }">
+                            <v-list-item-title v-bind="activatorProps">Удалить</v-list-item-title>
+                          </template>
+    
+                          <template v-slot:default="{ isActive }">
+                            <v-card title="Подтверждение">
+                              <v-card-text>
+                                Вы хотите удалить пользователя? Отменить действие будет невозможно.
+                              </v-card-text>
+    
+                              <v-card-actions>
+                                <v-spacer></v-spacer>
+
+                                <v-btn
+                                  color="blue"
+                                  text="Удалить"
+                                  @click="onDeleteClick(item.id)"
+                                  :loading="isLoading"
+                                />
+                                <v-btn
+                                  text="Отменить"
+                                  @click="isActive.value = false"
+                                />
+                              </v-card-actions>
+                            </v-card>
+                          </template>
+                        </v-dialog>
+                      </v-list-item>
+                    </v-list>
+                  </v-menu>
+                </v-col>
+              </v-row>
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts" setup>
+  interface User {
+    email: string;
+    full_name: string;
+    id: number;
+    phone: string;
+    status: 'ACTIVE';
+  }
+
+  const { data: users, refresh } = await useFetch('http://127.0.0.1:8008/api/users/', {
+    lazy: true,
+    server: false
+  })
+
+  const isLoading = ref(false)
+
+  const onDeleteClick = async (id: string) => {
+    isLoading.value = true
+    try {
+      const response = await $fetch(`http://127.0.0.1:8008/api/users/${id}/`, {
+        method: 'DELETE'
+      })
+    } catch(error) {
+      console.error(error)
+    } finally {
+      isLoading.value = false
+      refresh()
+    }
+  }
+</script>
